953356b1789b2dd0c46a0afe9caf71dffca214a2
Kārlis Kočāns <kk2822@gymn.local>
2021-11-26 09:23:15 +0200

    parbaudes darbs2

--- main.cpp
+++ main.cpp
@@ -1,16 +1,68 @@
 #include <iostream>
+#include <ctime>
+
 
 using namespace std;
 
+int masiva_funkcija(int x, int y, int z, int v);
+
 int main()
+    {
+int rin, kol, sak, bei;
+cout << "Ievadiet 2 skaitlus, kas noteiks masiva izmeru" << endl;
+cout << endl;
+cout << "Sis skaitlis noradis rindu skaitu (nedrikst but lielaks par 10): ";
+cin >> rin;
+cout << "Sis skaitlis noradis kollonu skaitune (nedrikst but lielaks par 10): ";
+cin >> kol;
+   if (rin,kol > 10)
+   {
+       cout << "Skaitlis nedrikst but lielaks par 10!" << endl;
+       return 0;
+    }
+    else
+    {
+
+    }
+
+cout << endl;
+cout << "Ievadiet skaitli, kas noteiks skaitla beigu diapozonu " << endl;
+cout << endl;
+sak = 0;
+cout << "Sis skaitlis noradis beigu diapozonu ";
+cin >> bei;
+
+masiva_funkcija(rin, kol, sak, bei);
+
+}
+
+int masiva_funkcija(int x,int y, int z, int v)
 {
-    int a,b,c,d;,0
-    cout << "Ludzu ievadiet 3 skaitlus!" << endl;
-    cin << a;
-    cin << b;
-    cin << n;
+   if (z > v)
+   {
+       cout << "Sakuma diapazona nedrikst but lielaka par beigam! " << endl;
+       return 0;
+   }
+   else
+   {
+
+   }
+    int mas [x][y];
+    srand(time(0));
+
+   for (int i = 0; i < x; i++)
+   {
+       for (int j = 0; j < y; j++)
+        {
+             mas[i][j];
+
+             mas[i][j] = rand()%((v+1)-z)+z;
+
+             cout << mas[i][j] << " ";
+        }
+    cout << endl;
+   }
 
 
     return 0;
-
 }
