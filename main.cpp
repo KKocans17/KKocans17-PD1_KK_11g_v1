7ca41b62007dae9c7489e54c51bac3b86bc7395f
Kārlis Kočāns <kk2822@gymn.local>
2021-11-26 09:01:54 +0200

    parbaudes darbs

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a,b,c,d;,0
+    cout << "Ludzu ievadiet 3 skaitlus!" << endl;
+    cin << a;
+    cin << b;
+    cin << n;
+
+
+    return 0;
+
+}
